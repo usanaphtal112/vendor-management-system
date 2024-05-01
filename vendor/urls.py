568d29426a09d3b06e45a395df638200d86c05d5
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-05-02 02:28:32 +0530

    AAdded Endpoint for Purchase order and Vendor

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,25 +1,30 @@
 from django.urls import path
-from . import views
+from .views import (
+    VendorListCreateAPIView,
+    VendorRetrieveUpdateDestroyAPIView,
+    PurchaseOrderListCreateAPIView,
+    PurchaseOrderRetrieveUpdateDestroyAPIView,
+)
 
 urlpatterns = [
     path(
         "vendors/",
-        views.VendorListCreateAPIView.as_view(),
+        VendorListCreateAPIView.as_view(),
         name="vendor-list-create",
     ),
     path(
         "vendors/<int:pk>/",
-        views.VendorRetrieveUpdateDestroyAPIView.as_view(),
+        VendorRetrieveUpdateDestroyAPIView.as_view(),
         name="vendor-detail",
     ),
     path(
         "purchase_orders/",
-        views.PurchaseOrderListCreateAPIView.as_view(),
+        PurchaseOrderListCreateAPIView.as_view(),
         name="purchaseorder-list-create",
     ),
     path(
         "purchase_orders/<int:pk>/",
-        views.PurchaseOrderRetrieveUpdateDestroyAPIView.as_view(),
+        PurchaseOrderRetrieveUpdateDestroyAPIView.as_view(),
         name="purchaseorder-detail",
     ),
 ]
